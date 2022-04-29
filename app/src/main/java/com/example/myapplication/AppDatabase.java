681f7eb2b093c560f5aff3f60638d41237053a14
lukkn <70594133+lukkn@users.noreply.github.com>
2022-04-29 16:15:56 -0400

    project final

--- app/src/main/java/com/example/myapplication/AppDatabase.java
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication;
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(version=1,entities = {Account.class})
-public abstract class AppDatabase extends RoomDatabase {
-    abstract UserDao userDao();
-}
